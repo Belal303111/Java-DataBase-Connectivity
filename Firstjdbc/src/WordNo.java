5b7c31340808f1f5f90c97b8f8224014e5ee4276
Belal <144605153+Belal303111@users.noreply.github.com>
2025-02-28 21:17:17 +0200

    A test program to Swing GUI

--- Firstjdbc/src/WordNo.java
+++ Firstjdbc/src/WordNo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import javax.swing.*;
 
 public class WordNo {
-    public static void main(String args[]){
+    public static void main(String args[]){ 
         JFrame f=new JFrame("text");
 
         Scanner s=new Scanner(System.in);
